0b44ce46cff423b3338cfddf3a0fa211462d5b0a
Crowdbotics <engineering@crowdbotics.com>
2023-01-26 00:06:17 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/wispy_rice_dev_59358/MainActivity.java
@@ -0,0 +1,15 @@
+package com.wispy_rice_dev_59358;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "wispy_rice_dev_59358";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wispy-rice-dev-59358",
+  url: "https://wispy-rice-dev-59358.botics.co",
+  api: "https://wispy-rice-dev-59358.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
